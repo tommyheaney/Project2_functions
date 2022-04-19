95bdb462b8fd22b8217b097a85dab87b5501fdae
tommyheaney <99109758+tommyheaney@users.noreply.github.com>
2022-04-19 20:04:53 +0100

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,23 @@
+// Changes initial script to Blue
+
+var orange = "Feeling Calm, ";
+var orange = orange.fontcolor("orange");
+document.write(orange);
+
+// Change document write from Dad to Son or Daughter will change the name away from Gordon on Frotend browser
+
+var Family = "McKenzie :) ", Dad = "Gordon :)  ", Son = "Paul :) ", Daughter = "Lucy :) ";
+document.write(Son);
+document.write("Have a nice day!");
+
+// Script for the Try it Button
+
+  function myFunction1() {
+      let text = document.getElementById("demo").innerHTML=Date();
+  }
+
+  function myfunction() {
+      var sentence = "I am learning";
+      sentence += " a lot from this course!";
+      document.getElementById("Concatenate").innerHTML = sentence;
+  }
